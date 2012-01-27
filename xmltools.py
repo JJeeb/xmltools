5b8dffe791ae5a89e6b2c1d11525a08c4d753915
Jean-Baptiste Potonnier <jean.baptiste.potonnier@gmail.com>
2012-01-27 18:18:23 +0100

    begin from_dict

--- xmltools.py
+++ xmltools.py
@@ -35,20 +35,22 @@ def to_dict(xmlElement):
 
 
 def from_dict(d):
+
+    def go(element, value):
+        if type(value) is str:
+            element.text = value
+        elif type(value) is list:
+            for child_value in value:
+                element.append(go(Element(tag=child_name), child_value))
+        elif type(value) is dict:
+            element.attrib = {name[1:]: value for name, value in value.items() if name[0] == '@'}
+            element.text = value['#text'] if '#text' in value else None
+
+            children = {name: value for name, value in value.items() if name[0] != '@' and name != '#text' }
+            for child_name, child_value in children.items():
+                element.append(go(Element(tag=child_name), child_value))
+        return element
+
     tagname = d.keys()[0]
-    element = Element(tagname)
-    tagvalue = d[tagname]
-    if type(tagvalue) is dict:
-        for name, value in tagvalue.items():
-            if name == '#text':
-                element.text = value
-            elif name[0] == '@':
-                element.attrib[name[1:]] = value
-            else:
-                subElement = Element(tag=name)
-                subElement.text = value
-                element.append(subElement)
-    else:
-        element.text = d[tagname]
-    return element
+    return go(Element(tag=tagname), d[tagname])
 
